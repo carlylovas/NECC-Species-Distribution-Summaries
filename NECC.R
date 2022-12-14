6ca10c1329049361d6e6d4ec07e5e0960beebfb8
carlylovas <clovas@gmri.org>
2022-12-14 14:28:38 -0500

    code

---
+++ NECC.R
@@ -0,0 +1,42 @@
+library(gmRi)
+
+
+#load NFMS Trawl Survey
+clean_survey<-gmri_survdat_prep(
+  survdat_source ="most recent",
+  box_location ="cloudstorage"
+)
+str(clean_survey)
+
+#load NECC Species List
+NECC<-read.csv("speciesList_inNECC.csv", header=TRUE)
+NECC<-NECC %>%
+  rename(comname = Species_comnam)
+NECC <- tolower(NECC$comname)
+head(NECC)
+
+#Filter trawl by species
+NECC_fishes<-clean_survey %>% 
+  select("comname", "est_year", "biomass_kg", "decdeg_beglat",  "decdeg_beglon", "season", "survey_area") %>%
+  filter(comname %in% NECC)
+summary(NECC_fishes)
+
+
+#center of latitude over time?
+
+dogfish_fall<- NECC_fishes %>%
+  filter(comname == "smooth dogfish") %>%
+  filter(season == "Fall") %>%
+  mutate(weightedLat = weighted.mean(x = decdeg_beglat, w=biomass_kg)) %>%
+  mutate(weightedLon = weighted.mean(x= decdeg_beglon, w=biomass_kg))
+
+ggplot() +
+  geom_point(data=dogfish_fall, aes(x=est_year, y=weightedLa, size=biomass_kg)) +
+  geom_abline()
+#okay? maybe review Lindsey trawl code
+##calc center of lat for time series?
+
+
+
+
+
